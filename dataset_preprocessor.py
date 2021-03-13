d3b7883a9f747ef0ef642bf2a2f385699b4480cf
Shielou Estrada <shielou@gmail.com>
2021-03-13 17:09:40 +0800

    Updated cleanup

--- dataset_preprocessor.py
+++ dataset_preprocessor.py
@@ -1,13 +1,18 @@
 import numpy as np
 import pandas as pd
 import os
-from pandas.api.types import union_categoricals
 
 pd.set_option('display.max_columns', 50)
 pd.set_option('display.width', 1000)
 
-ENTRY_KEY = 'entry'
-PREFCODE_KEY = 'prefcode'
+kHSCODE = 'hscode'
+kCOUNTRY_ORIGIN = 'countryorigin_iso3'
+kCOUNTRY_EXPORT = 'countryexport_iso3'
+kENTRY = 'entry'
+kPREFCODE = 'prefcode'
+kCURRENCY = 'currency'
+kPORT = 'port'
+CATEGORY = 'category'
 
 
 class DatasetPreprocessor:
@@ -21,18 +26,21 @@ class DatasetPreprocessor:
       # If the pickle file is not present then read each csv file and save to the pickle file
       if force_read or not os.path.exists(self.boc_lite_all_file):
          self.df_all = self.read_all_years()
+         self.df_all.to_pickle(self.boc_lite_all_file)
       else:
          self.df_all = pd.read_pickle(self.boc_lite_all_file)
-      print(self.df_all.dtypes)
-      print(self.df_all.describe(include='all'))
 
-   # Utility function for checking if a value is a float
-   def check_float(self, value):
-      try:
-         float(value)
-         return np.NaN
-      except ValueError:
-         return value
+      # print('*** dtypes before cleanup ***')
+      # print(self.df_all.dtypes)
+      # print('*** summary before cleanup ***')
+      # print(self.df_all.describe(include='all'))
+
+      self.cleanup()
+
+      print('*** dtypes after cleanup ***')
+      print(self.df_all.dtypes)
+      print('*** summary after cleanup ***')
+      print(self.df_all.describe(include='all'))
 
    # Reads all the datasets for all years
    def read_all_years(self):
@@ -40,14 +48,14 @@ class DatasetPreprocessor:
 
       # Read the datasets for all years
       # Data types for the columns
-      dtypes = {'hscode': str,
-                'countryorigin_iso3': str,
-                'countryexport_iso3': str,
-                ENTRY_KEY: 'category',
-                PREFCODE_KEY: 'category',
-                'currency': str,
+      dtypes = {kHSCODE: CATEGORY,
+                kCOUNTRY_ORIGIN: CATEGORY,
+                kCOUNTRY_EXPORT: CATEGORY,
+                kENTRY: CATEGORY,
+                kPREFCODE: CATEGORY,
+                kCURRENCY: CATEGORY,
+                kPORT: CATEGORY,
                 'uid': str,
-                'port': str,
                 'dutiablevaluephp': np.float,
                 'dutypaid': np.float,
                 'exciseadvalorem': np.float,
@@ -55,7 +63,6 @@ class DatasetPreprocessor:
                 'vatpaid': np.float
                 }
       df_list = list()
-      #prefcode_cols = list()
       for y in np.nditer(self.years):
          print(f'*** YEAR {y} ***')
          # Read the csv file
@@ -79,27 +86,17 @@ class DatasetPreprocessor:
          # print(f"*** START YEAR {y} ***")
          # print(df_y.head(n=20))
          # print(df_y.dtypes)
-         # print(df_y[PREFCODE_KEY].cat.categories)
+         # print(df_y[kPREFCODE].cat.categories)
          # print(df_y.describe(include='all'))
          # print(f"*** END YEAR {y} ***")
 
          df_list.append(df_y)
-         #prefcode_cols.append(df_y[PREFCODE_KEY])
-
-      # uc = union_categoricals(prefcode_cols)
-      # uc.add_categories('NOCODE', inplace=True)
-      # for df_y in df_list:
-      #    df_y[PREFCODE_KEY] = pd.Categorical(df_y[PREFCODE_KEY], categories=uc.categories)
 
       # Merge all years' data into 1 dataframe
       df_all_years = pd.concat(df_list, copy=False)
       # print(f"*** START ALL YEARS ***")
       # print(df_all.head(n=20))
       # print(f"*** END ALL YEARS ***")
-      df_all_years.to_pickle(self.boc_lite_all_file)
-
-      # print('*** prefcode categories before ***')
-      # print(df_all_years[PREFCODE_KEY].cat.categories)
 
       return df_all_years
 
@@ -111,23 +108,66 @@ class DatasetPreprocessor:
 
       print('*** CLEANUP ***')
       # 'prefcode' column: change "" values to "NOCODE" and convert 'prefcode' to categorical variable
-      self.df_all[PREFCODE_KEY].fillna('NOCODE', inplace=True)
-      self.df_all[PREFCODE_KEY] = self.df_all[PREFCODE_KEY].astype('category')
-      print('*** prefcode ***')
-      print(self.df_all[PREFCODE_KEY].describe())
-      print(self.df_all[PREFCODE_KEY].cat.categories)
+      self.df_all[kPREFCODE].fillna('NOCODE', inplace=True)
+      self.make_categorical(kPREFCODE)
 
       # Restrict the sample to consumption imports, as opposed to warehousing and transshipment
       # imports, because Philippine duties and taxes are levied on consumption imports under customs law.
-      self.df_all[ENTRY_KEY] = self.df_all[ENTRY_KEY].astype('category')
-      self.df_all = self.df_all.loc[self.df_all[ENTRY_KEY].isin(['C', ''])]
-      print('*** entry ***')
-      print(self.df_all[ENTRY_KEY].describe())
-      print(self.df_all[ENTRY_KEY].cat.categories)
+      self.make_categorical(kENTRY)
+      self.df_all = self.df_all.loc[self.df_all[kENTRY].isin(['C', ''])]
+      print(self.df_all[kENTRY].describe())
 
-      print(self.df_all.dtypes)
+      # Check that 'hscode' strings are always 11 characters
+      print('*** Cleanup hscode column ***')
+      print(self.df_all[kHSCODE].str.len().describe())
+      # self.df_all[kHSCODE] = self.df_all[kHSCODE].str.pad(11, side='left', fillchar='0')
+      self.make_categorical(kHSCODE)
+      # 15 Categories to exclude under Valuation Reforms 2014-2015
+      # 7207, 7208, 7209, 7210, 7216, 7225, 7227, 7228, 1601, 1602, 3901, 3902, 3903, 3904, 3907
+      regex_str = '^(7207|7208|7209|7210|7216|7225|7227|7228|1601|1602|3901|3902|3903|3904|3907)'
+      to_drop = self.df_all.loc[self.df_all[kHSCODE].str.contains(regex_str, regex=True)]
+      #to_drop.to_csv('./hscode_todrop.csv', columns=[kHSCODE])
+      to_drop_indices = to_drop.index
+      print(f'shape before cleanup: {self.df_all.shape}, indices count: {len(to_drop_indices)}')
+      self.df_all = self.df_all.drop(index=to_drop_indices)
+      print(f'shape after cleanup: {self.df_all.shape}')
+
+      # Force other categorical columns
+      self.make_categorical(kCOUNTRY_ORIGIN)
+      self.make_categorical(kCOUNTRY_EXPORT)
+      self.make_categorical(kCURRENCY)
+      self.make_categorical(kPORT)
+
+      # Read files used for JOINING homogeneous reference-priced products
+      rauch_sitc2 = pd.read_csv('./Rauch_classification_revised.txt', sep='\t',
+                                dtype={'sitc4': int, 'con': str, 'lib': str})
+      hs2017_sitc2 = pd.read_csv('./HS2017toSITC2ConversionAndCorrelationTables.txt', sep='\t',
+                                 dtype={'From HS 2017': str, 'To SITC Rev. 2': int})
+      # Inner join 2 read files
+      merged_sitc2 = pd.merge(hs2017_sitc2, rauch_sitc2, how='left', left_on='To SITC Rev. 2', right_on='sitc4')
+      merged_sitc2.to_csv('./merged_sitc2.csv')
+      print(rauch_sitc2.describe(include='all'))
+      print(hs2017_sitc2.describe(include='all'))
+      print(merged_sitc2.describe(include='all'))
+      self.df_all = self.df_all.loc[self.df_all[kENTRY].isin(['C', ''])]
+
+      # *** Utility functions ***
+
+   # For checking if a value is a float
+   def check_float(self, value):
+      try:
+         float(value)
+         return np.NaN
+      except ValueError:
+         return value
+
+   # Make a column's dtype categorical
+   def make_categorical(self, col_key):
+      self.df_all[col_key] = self.df_all[col_key].astype(CATEGORY)
+      print(f'*** make_categorical: {col_key} ***')
+      print(self.df_all[col_key].describe())
+      print(self.df_all[col_key].cat.categories)
 
 
 if __name__ == "__main__":
    pp = DatasetPreprocessor(force_read=False)
-   pp.cleanup()
